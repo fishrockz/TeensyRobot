39c20c58043bcd27331638fdb72068c07fac1bee
fishrockz <fishrockz@users.noreply.github.com>
2016-11-28 18:12:20 +0000

    quick commit test as i add some more simple stuff

--- weaponTesting/weaponTesting.ino
+++ weaponTesting/weaponTesting.ino
@@ -7,6 +7,17 @@ const size_t BUF_DIM = 32768;
 // 8 MiB file.
 const uint32_t FILE_SIZE = 256UL*BUF_DIM;
 
+const uint32_t PPMPin = 10;
+const uint32_t boardLEDPin = 13;
+
+const uint32_t ValveAPin = 13;
+const uint32_t ValveBPin = 13;
+const uint32_t ValveCPin = 13;
+const uint32_t ValveDPin = 13;
+const uint32_t ValveEPin = 13;
+
+
+
 
 PulsePositionInput RadioIn;
 SdFatSdioEX sd;
@@ -16,8 +27,15 @@ File file;
 
 
 void setup() {
-pinMode(13,OUTPUT);  
-RadioIn.begin(10);
+pinMode(boardLEDPin,OUTPUT);  
+RadioIn.begin(PPMPin);
+
+pinMode(ValveAPin,OUTPUT); 
+pinMode(ValveBPin,OUTPUT); 
+pinMode(ValveCPin,OUTPUT); 
+pinMode(ValveDPin,OUTPUT); 
+pinMode(ValveEPin,OUTPUT); 
+
 }
 
 
